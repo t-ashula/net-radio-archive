38980750fc6acdebd3b34784ff6c5ff94c9e664b
t-ashula <office@ashula.info>
2015-10-12 20:31:58 +0900

    fix: change CreateAnitamaProgram migration

--- db/migrate/20150521174524_create_anitama_program.rb
+++ db/migrate/20150521174524_create_anitama_program.rb
@@ -1,24 +1,19 @@
 class CreateAnitamaProgram < ActiveRecord::Migration
   def up
     # book_id がUNIQUEになることに確信を持てないのでこういう設計に
-    sql = <<EOF
-CREATE TABLE `anitama_programs` (
-    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `book_id` varchar(250) CHARACTER SET ASCII NOT NULL,
-    `title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
-    `update_time` datetime NOT NULL,
-    `state` varchar(100) CHARACTER SET ASCII NOT NULL,
-    `retry_count` int UNSIGNED NOT NULL,
-    `created_at` datetime NOT NULL,
-    `updated_at` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY (`book_id`, `update_time`)
-);
-EOF
-    ActiveRecord::Base.connection.execute(sql)
+    create_table 'anitama_programs', force: true, unsigned: true do |t|
+      t.column 'book_id',     :string,   null: false, limit: 250, charset: 'ascii'
+      t.column 'title',       :string,   null: false, limit: 250, charset: 'utf8mb4'
+      t.column 'update_time', :datetime, null: false
+      t.column 'state',       :string,   null: false, limit: 100, charset: 'ascii'
+      t.column 'retry_count', :integer,  null: false, unsigned: true
+      t.timestamps null: false
+
+      t.index ['book_id', 'update_time'], unique: true, name: 'book_id'
+    end
   end
+
   def down
-    sql = 'DROP TABLE `anitama_programs`;'
-    ActiveRecord::Base.connection.execute(sql)
+    drop_table 'anitama_programs'
   end
 end
