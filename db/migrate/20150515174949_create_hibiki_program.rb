51c361b107e6bb80f2c75057200b4fe480822581
t-ashula <office@ashula.info>
2015-10-12 20:16:16 +0900

    fix: change CreateHibikiProgram migration

--- db/migrate/20150515174949_create_hibiki_program.rb
+++ db/migrate/20150515174949_create_hibiki_program.rb
@@ -1,23 +1,18 @@
 class CreateHibikiProgram < ActiveRecord::Migration
   def up
-    sql = <<EOF
-CREATE TABLE `hibiki_programs` (
-    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
-    `comment` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
-    `rtmp_url` varchar(767) CHARACTER SET ASCII NOT NULL,
-    `state` varchar(100) CHARACTER SET ASCII NOT NULL,
-    `retry_count` int UNSIGNED NOT NULL,
-    `created_at` datetime NOT NULL,
-    `updated_at` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY (`rtmp_url`)
-);
-EOF
-    ActiveRecord::Base.connection.execute(sql)
+    create_table 'hibiki_programs', force: true, unsigned: true do |t|
+      t.column 'title',       :string,   null: false, limit: 250, charset: 'utf8mb4'
+      t.column 'comment',     :string,   null: false, limit: 150, charset: 'utf8mb4'
+      t.column 'rtmp_url',    :string,   null: false, limit: 767, charset: 'ascii'
+      t.column 'state',       :string,   null: false, limit: 100, charset: 'ascii'
+      t.column 'retry_count', :integer,  null: false, unsigned: true
+      t.timestamps null: false
+      
+      t.index 'rtmp_url', unique: true, name: 'rtmp_url'
+    end
   end
+  
   def down
-    sql = 'DROP TABLE `hibiki_programs`;'
-    ActiveRecord::Base.connection.execute(sql)
+    drop_table 'hibiki_programs'
   end
 end
