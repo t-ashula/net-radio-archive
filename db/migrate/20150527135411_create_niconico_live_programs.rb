72b30b73ab6a260420d8a9f53090eb1ed4d22229
t-ashula <office@ashula.info>
2015-10-12 21:52:05 +0900

    fix: add SQLite3 support for CreateNiconicoLivePrograms migration

--- db/migrate/20150527135411_create_niconico_live_programs.rb
+++ db/migrate/20150527135411_create_niconico_live_programs.rb
@@ -1,13 +1,23 @@
 class CreateNiconicoLivePrograms < ActiveRecord::Migration
   def up
-    create_table 'niconico_live_programs', force: true, unsigned: true do |t|
+    adapter = connection.adapter_name.downcase
+    create_table 'niconico_live_programs', force: true, id: false do |t|
+      if adapter == 'sqlite'
+        t.column 'id', 'BIGINT PRIMARY KEY NOT NULL'
+      else
+        t.column 'id', :integer, null: false, limit: 8, unsigned: true
+      end
       t.column 'title',       :string,  null: false, limit: 250, charset: 'utf8mb4'
       t.column 'state',       :string,  null: false, limit: 100, charset: 'ascii'
       t.column 'retry_count', :integer, null: false, unsigned: true
       t.timestamps null: false
     end
 
-    change_column 'niconico_live_programs', 'id', :integer, limit: 8, unsigned: true, auto_increment: false
+    if adapter == 'sqlite'
+      # nothing
+    else
+      execute 'ALTER TABLE niconico_live_programs ADD PRIMARY KEY (id);'
+    end
   end
   
   def down
