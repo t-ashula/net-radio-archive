e090bc8e6b916f454a4f884b635547e583c07782
t.ashula <dev@ashula.info>
2015-11-16 04:19:56 +0900

    rewrite SQL

--- db/migrate/20151030150712_add_column_onsen_retry.rb
+++ db/migrate/20151030150712_add_column_onsen_retry.rb
@@ -1,13 +1,9 @@
 class AddColumnOnsenRetry < ActiveRecord::Migration
   def up
-    sql = <<EOF
-ALTER TABLE `onsen_programs`
-ADD COLUMN `retry_count` int UNSIGNED NOT NULL
-AFTER `state`
-EOF
-    ActiveRecord::Base.connection.execute(sql)
+    add_column 'onsen_programs', 'retry_count', :integer, null: false, unsigned: true, after: 'state', default: 0
   end
 
   def down
+    # remove_column 'onsen_programs', 'retry_count'
   end
 end
