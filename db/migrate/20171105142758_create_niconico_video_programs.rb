67a6de7808a044d7334d0518b9cb5c5c8ccfc0a9
t.ashula <dev@ashula.info>
2017-11-08 21:40:18 +0900

    use AR dsl

--- db/migrate/20171105142758_create_niconico_video_programs.rb
+++ db/migrate/20171105142758_create_niconico_video_programs.rb
@@ -1,23 +1,17 @@
 class CreateNiconicoVideoPrograms < ActiveRecord::Migration
   def up
-    sql = <<EOF
-CREATE TABLE `niconico_video_programs` (
-    `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    `video_id` varchar(250) CHARACTER SET ASCII NOT NULL,
-    `title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
-    `state` varchar(100) CHARACTER SET ASCII NOT NULL,
-    `retry_count` int UNSIGNED NOT NULL,
-    `created_at` datetime NOT NULL,
-    `updated_at` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY (`video_id`)
-);
-EOF
-    ActiveRecord::Base.connection.execute(sql)
+    create_table 'niconico_video_programs', force: true, unsigned: true do |t|
+      t.column 'video_id', :string, null: false, limit: 250, charset: 'ascii'
+      t.column 'title',    :string, null: false, limit: 250, charset: 'utf8mb4'
+      t.column 'state',    :string, null: false, limit: 100, charset: 'ascii'
+      t.column 'retry_count', :integer, null: false
+      t.timestamps null: false
+
+      t.index 'video_id', unique: true
+    end
   end
 
   def down
-    sql = 'DROP TABLE `niconico_video_programs`;'
-    ActiveRecord::Base.connection.execute(sql)
+    drop_table 'niconico_video_programs'
   end
 end
